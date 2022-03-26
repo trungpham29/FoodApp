3eb724a6723dcf10ae58a784404e109620749aef
trungpham29 <pttrung.18it2@vku.udn.vn>
2022-03-26 13:28:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+food_app
+
+A new Flutter project.
+
+"# FoodApp" 
+"# FoodApp" 
---
+++ lib/screens/my_profile/my_profile.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/auth/sign_in.dart';
+import 'package:food_app/config/colors.dart';
+import 'package:food_app/models/user_model.dart';
+import 'package:food_app/providers/user_provider.dart';
+import 'package:food_app/screens/check_out/delivery_details/delivery_details.dart';
+import 'package:food_app/screens/home/drawer_side.dart';
+import 'package:food_app/screens/review_cart/review_cart.dart';
+
+class MyProfile extends StatefulWidget {
+  UserProvider userProvider;
+  MyProfile({this.userProvider});
+
+  @override
+  _MyProfileState createState() => _MyProfileState();
+}
+
+class _MyProfileState extends State<MyProfile> {
+  @override
+  Widget listTile({IconData icon, String title, Function onTap}) {
+    return Column(
+      children: [
+        Divider(
+          height: 1,
+        ),
+        ListTile(
+          onTap: onTap,
+          leading: Icon(icon),
+          title: Text(title),
+          trailing: Icon(Icons.arrow_forward_ios),
+        )
+      ],
+    );
+  }
+
+  Widget build(BuildContext context) {
+    var userData = widget.userProvider.currentUserData;
+
+    return Scaffold(
+      backgroundColor: primaryColor,
+      appBar: AppBar(
+        backgroundColor: Color(0xffd1ad17),
+        elevation: 0.0,
+        title: Text(
+          "My Profile",
+          style: TextStyle(
+            fontSize: 18,
+            color: textColor,
+          ),
+        ),
+      ),
+      drawer: DrawerSide(
+        userProvider: widget.userProvider,
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 100,
+                  color: primaryColor,
+                ),
+                Container(
+                  height: 548,
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: scaffoldBackgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: ListView(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            width: 250,
+                            height: 80,
+                            padding: EdgeInsets.only(left: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      userData.userName,
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                          color: textColor),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(userData.userEmail),
+                                  ],
+                                ),
+                                CircleAvatar(
+                                  radius: 15,
+                                  backgroundColor: primaryColor,
+                                  child: CircleAvatar(
+                                    radius: 12,
+                                    child: Icon(
+                                      Icons.edit,
+                                      color: primaryColor,
+                                    ),
+                                    backgroundColor: scaffoldBackgroundColor,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      listTile(
+                        icon: Icons.shop_outlined,
+                        title: "My Orders",
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => ReviewCart(),
+                            ),
+                          );
+                        },
+                      ),
+                      listTile(
+                        icon: Icons.location_on_outlined,
+                        title: "My Delivery Address",
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => DeliveryDetails(),
+                            ),
+                          );
+                        },
+                      ),
+                      listTile(
+                          icon: Icons.person_outline, title: "Refer A Friends"),
+                      listTile(
+                          icon: Icons.file_copy_outlined,
+                          title: "Terms & Conditions"),
+                      listTile(
+                          icon: Icons.policy_outlined, title: "Privacy Policy"),
+                      listTile(icon: Icons.add_chart, title: "About"),
+                      listTile(
+                        icon: Icons.exit_to_app_outlined,
+                        title: "Log Out",
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => SignIn(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 30),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: primaryColor,
+              child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    userData.userImage ??
+                        "https://s3.envato.com/files/328957910/vegi_thumb.png",
+                  ),
+                  radius: 45,
+                  backgroundColor: scaffoldBackgroundColor),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
